669c479a7135eca7b9641f8b73b765219f90185e
ShestakovStanislav <w3orks@mail.ru>
2021-04-07 22:26:54 +0300

    Object example

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-const name = 'Fight...';
-function getMessage(name, lastName){
-    console.log(name + ' ' + lastName +' Fight....');
-}
-getMessage('Sonya', 'SubZero');
-getMessage(name);
+const player1 = {
+    name:'Scorpion',
+    hp: 100,
+    'string': true,
+    100: true,
+    attack: function(){
+        console.log('Fight....');
+    }
+};
+player1.attack();
+
+console.log(player1);
+console.log(player1.name);
+console.log(player1['hp']);
+console.log(player1['string']);
+
+const  count = 100;
+console.log(player1[count]);
 
 
-function  createFullName(name, lastName, secondName){
-    console.log(33)
-    const result = name + ' ' +secondName +' ' + lastName;
-    return result;
-    console.log(1)
-}
+player1.weapon = 'Kunai';
+console.log(player1);
 
-const fullname = createFullName('Zar','Zakharov', 'Konstantinovich');
-console.log(fullname);
+player1.hp = player1.hp-20;
+console.log(player1.hp);
+
+delete  player1.hp;
+console.log(player1);
