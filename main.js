69fa746bf5dda016a372ac2db5b137f98a8d9305
ShestakovStanislav <w3orks@mail.ru>
2021-04-07 22:33:09 +0300

    array example

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-const player1 = {
-    name:'Scorpion',
-    hp: 100,
-    'string': true,
-    100: true,
-    attack: function(){
-        console.log('Fight....');
-    }
-};
-player1.attack();
+const message = ['Hello',true,2021];
+console.log(message);
+console.log(message[1]);
 
-console.log(player1);
-console.log(player1.name);
-console.log(player1['hp']);
-console.log(player1['string']);
+message.push('World');
 
-const  count = 100;
-console.log(player1[count]);
-
-
-player1.weapon = 'Kunai';
-console.log(player1);
-
-player1.hp = player1.hp-20;
-console.log(player1.hp);
-
-delete  player1.hp;
-console.log(player1);
+const str = message[0] +' ' + message[3];
+console.log(str);
