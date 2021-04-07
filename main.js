b5d635e1cc3210fb680f2523256e2ea59595f8ac
ShestakovStanislav <w3orks@mail.ru>
2021-04-07 22:14:26 +0300

    function

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let firstName;
-firstName = 'Scorpion';
-console.log(typeof firstName);
-console.log(typeof +'5');
-console.log(Boolean(0));
-console.log(Boolean(!!''));
+const name = 'Fight...';
+function getMessage(name, lastName){
+    console.log(name + ' ' + lastName +' Fight....');
+}
+getMessage('Sonya', 'SubZero');
+getMessage(name);
+
+
+function  createFullName(name, lastName, secondName){
+    console.log(33)
+    const result = name + ' ' +secondName +' ' + lastName;
+    return result;
+    console.log(1)
+}
+
+const fullname = createFullName('Zar','Zakharov', 'Konstantinovich');
+console.log(fullname);
