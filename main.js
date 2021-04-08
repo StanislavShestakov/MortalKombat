fa0ae07f12d93b952726e3bd23c3d00d79febcd6
ShestakovStanislav <w3orks@mail.ru>
2021-04-08 21:17:12 +0300

    game add Players

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
-const message = ['Hello',true,2021];
-console.log(message);
-console.log(message[1]);
+const player1 = {
+    name:'Scorpion',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon:['chain','thorn'],
+    attack: function(){
+        console.log(this.name + ' Fight.... ');
+    }
+};
+const player2 = {
+    name:'SubZero',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon:['cold','water'],
+    attack: function(){
+        console.log(this.name + ' Fight.... ');
+    }
+};
+console.log(player1);
+player1.attack();
+function createPlayer(id,name,hp,img){
+    const $player1 = document.createElement('div');
+    $player1.classList.add(id);
 
-message.push('World');
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    $life.style.width = hp + '%';
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $name.innerText = name;
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
 
-const str = message[0] +' ' + message[3];
-console.log(str);
+    const $character = document.createElement('div');
+    const $img = document.createElement('img');
+    $img.src = img;
+    $character.appendChild($img);
+    $character.classList.add('character');
+
+    $player1.appendChild($progressbar);
+    $player1.appendChild($character);
+
+    const $root = document.querySelector('.root .arenas');
+    $root.appendChild($player1);
+}
+createPlayer('player1','Scorpion',100,'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif');
+createPlayer('player2','SubZero',80,'http://reactmarathon-api.herokuapp.com/assets/subzero.gif');
