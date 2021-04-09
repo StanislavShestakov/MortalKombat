d2d0dd068290f467fcbd10f6002503a8b54013d9
admin <w3orks@mail.ru>
2021-04-09 13:38:53 +0300

    add 5 figters

--- main.js
+++ main.js
@@ -16,26 +16,51 @@ const player2 = {
         console.log(this.name + ' Fight.... ');
     }
 };
-console.log(player1);
-player1.attack();
-function createPlayer(id,name,hp,img){
+const player3 = {
+    name:'SubZero',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+    weapon:['cold','water'],
+    attack: function(){
+        console.log(this.name + ' Fight.... ');
+    }
+};
+const player4 = {
+    name:'SubZero',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+    weapon:['cold','water'],
+    attack: function(){
+        console.log(this.name + ' Fight.... ');
+    }
+};
+const player5 = {
+    name:'SubZero',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+    weapon:['cold','water'],
+    attack: function(){
+        console.log(this.name + ' Fight.... ');
+    }
+};
+function createPlayer(str,player){
     const $player1 = document.createElement('div');
-    $player1.classList.add(id);
+    $player1.classList.add(str);
 
     const $progressbar = document.createElement('div');
     $progressbar.classList.add('progressbar');
     const $life = document.createElement('div');
     $life.classList.add('life');
-    $life.style.width = hp + '%';
+    $life.style.width = player.hp + '%';
     const $name = document.createElement('div');
     $name.classList.add('name');
-    $name.innerText = name;
+    $name.innerText = player.name;
     $progressbar.appendChild($life);
     $progressbar.appendChild($name);
 
     const $character = document.createElement('div');
     const $img = document.createElement('img');
-    $img.src = img;
+    $img.src = player.img;
     $character.appendChild($img);
     $character.classList.add('character');
 
@@ -45,5 +70,8 @@ function createPlayer(id,name,hp,img){
     const $root = document.querySelector('.root .arenas');
     $root.appendChild($player1);
 }
-createPlayer('player1','Scorpion',100,'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif');
-createPlayer('player2','SubZero',80,'http://reactmarathon-api.herokuapp.com/assets/subzero.gif');
+createPlayer('player1',player1);
+createPlayer('player2',player2);
+createPlayer('player3',player3);
+createPlayer('player4',player4);
+createPlayer('player5',player5);
