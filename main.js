4cf2e51b7f2ccd0e0d99801186705910dccae06d
ShestakovStanislav <w3orks@mail.ru>
2021-04-10 23:08:35 +0300

    use optional iperator

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-const walkDirection = prompt('Where are we going?');
+console.log(true && false);
+console.log(true && true);
 
-if(walkDirection === 'left'){
-    console.log('We shoud go on the ' + walkDirection);
-}else if(walkDirection === 'right'){
-    console.log('We shoud go on the ' + walkDirection);
-}
-else {
-    console.log('We shold stay!');
-}
+console.log(true && 'This string');
+console.log(true && 'This string' && true);
+
+console.log(false || true);
+console.log(false || false);
+
+console.log(!false);
+console.log(!true);
+
+console.log(!'string');
+console.log(!1);
+console.log(!'');
+
+console.log(true?1:2);
+console.log(false?1:2);
+
+const isTrue = true? 'yes':'no';
+console.log(isTrue);
+
+
+
+
+
+
