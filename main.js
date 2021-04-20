303e993a044ac4b4910723eacb6c5b58175300fa
ShestakovStanislav <w3orks@mail.ru>
2021-04-20 23:09:44 +0300

    arrow function

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
-// function  sum(a,b){
-//     return a+b;
-// }
-
-const sum =function (a =0,b=0){
-    return a+b;
-}
-
-console.log(sum(4,2));
-
-const calc =sum;
-
-console.log(calc(4,2));
-console.log(calc());
-
-const a = {
-    name: 'Zar',
+const sum = (a,b) => a+b;
+const sum1 = (a,b) => {
+    console.log('Function Sum');
+   return  a+b;
 };
 
-(function (){
-    console.log(this)
-}).call(a);
+console.log(sum(5,2));
+console.log(sum1(5,2));
+
+const random =(num) => Math.ceil(Math.random()*num);
+console.log(random(100));
+//-------------------------------------------------------------
+
+console.log('------------------------------------------');
+
+const obj1 = {
+    name:'Stas',
+    fn: function (){
+        console.log(this);
+    }
+}
+
+const obj2 = {
+    name:'Stas Shestakov',
+    fn: () => console.log(this),
+}
+const obj3 = {
+    name:'Stas Shestakov',
+    fn: function (){
+        const a = () => console.log(this);
+        a();
+    }
+
+}
+
+obj1.fn();
+obj2.fn();
+obj3.fn();
+
+const copy1 = obj1.fn;
+const copy2 = obj2.fn;
+const copy3 = obj3.fn;
+const copy4 = obj3.fn.bind(obj3);
+
+
+copy1();
+copy2();
+copy3();
+copy4();
