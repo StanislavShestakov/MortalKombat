bf1385ca7507d43d79ff198e465e5b7329725847
ShestakovStanislav <w3orks@mail.ru>
2021-04-22 23:11:21 +0300

    class1\

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-function player(name,age){
-    this.name = name;
-    this.age = age;
+class Player {
+    constructor(props) {
+        console.log(props);
+        this.name = props.name;
+        this.hp = props.hp;
+        this.type = props.type;
+    }
+    whoop = () => {
+        console.log(`${this.name} let's fight!`);
+    }
 }
 
-const player1= new player('Stas','35');
-const player2= new player('Vasya','35');
+const player1 = new Player({
+    name: 'Scorpion',
+    hp: 100,
+    type: 'fighters',
+});
+
 console.log(player1);
+
+class Player1 extends Player {
+    constructor(props) {
+        super(props);
+        this.winners = props.winners;
+    }
+
+    whoop = () => {
+        console.log('This is new WHOOP');
+    }
+}
+
+const player2 = new Player1({
+    name: 'Kitana',
+    hp: 100,
+    type: 'fighters',
+    winners: true,
+});
+
 console.log(player2);
+console.log(player2.whoop());
